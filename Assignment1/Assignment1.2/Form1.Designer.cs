c7a8c9e0bd3afd068e965ed0094dec481fe57165
BingHaoChuah <104841892@student.swin.edu.au>
2025-08-24 10:02:48 +1000

    Added Deployment task

---
+++ Assignment1/Assignment1.2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Assignment1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.lblNumCount = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(153, 266);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(136, 102);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(475, 266);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(136, 102);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "Subtract";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // lblNumCount
+            // 
+            this.lblNumCount.AutoSize = true;
+            this.lblNumCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumCount.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNumCount.Location = new System.Drawing.Point(363, 112);
+            this.lblNumCount.Name = "lblNumCount";
+            this.lblNumCount.Size = new System.Drawing.Size(46, 52);
+            this.lblNumCount.TabIndex = 2;
+            this.lblNumCount.Text = "0";
+            this.lblNumCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTimer.Location = new System.Drawing.Point(229, 28);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(46, 52);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.Text = "0";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblNumCount);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Positive vs Negative";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Label lblNumCount;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ Assignment1/Assignment1.2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1._2
+{
+    public partial class Form1 : Form
+    {
+        int max_time = 60; // Maximum amount of time on the timer
+        int time_left = 60; // The amount of time left on the timer
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Add button: Add 1 to the counter once clicked. Only works while timer is not zero
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (time_left > 0)
+            {
+                int num = int.Parse(lblNumCount.Text);
+                num++;
+                lblNumCount.Text = num.ToString();
+            }
+        }
+
+        // Subtract button: Subtract 1 to the counter once clicked. Only works while timer is not zero
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            if (time_left > 0)
+            {
+                int num = int.Parse(lblNumCount.Text);
+                num--;
+                lblNumCount.Text = num.ToString();
+            }
+        }
+
+        // Update every second the timer tool ticked
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time_left >= 0 && timer1.Enabled)
+            {
+                // Count down the timer label every second from the max time to zero
+                lblTimer.Text = time_left.ToString() + " seconds left";
+                time_left--;
+            }
+            else
+            {
+                // Once the timer reaches 0, a messagebox will appear announcing the winner or draw
+
+                DialogResult retryResult;
+                timer1.Enabled = false;
+                int num = int.Parse(lblNumCount.Text);
+                if (num > 0)
+                {
+                    // Positive number won
+                    retryResult = MessageBox.Show(
+                        $"Positive number won. {num} is a positive number. Do you want to play again?",
+                        "Game Ended",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information                        
+                        );
+                }
+                else if (num < 0)
+                {
+                    // Negative number won
+                    retryResult = MessageBox.Show(
+                        $"Negative number won. {num} is a negative number. Do you want to play again?",
+                        "Game Ended",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information
+                        );
+                }
+                else
+                {
+                    // Draw
+                    retryResult = MessageBox.Show(
+                        $"It's a draw. {num} is a neutral number. Do you want to play again?",
+                        "Game Ended",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information
+                        );
+                }
+
+                // Play again action: There will be a yes or no options in the messagebox to retry the game or not.
+                if (retryResult == DialogResult.Yes)
+                {
+                    // Reset the game if yes
+                    timer1.Enabled = true;
+                    time_left = max_time;
+                    lblNumCount.Text = "0";
+                }
+                else
+                {
+                    // Close the game if no
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
